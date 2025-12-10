8503154b89e93070958a580e6d669539f38349f8
Sulaiman Khydyr uulu <sulaiman.khydyruulu@duke.edu>
2025-12-10 12:20:09 -0500

    Implement longestCommonPrefix method in Solution class
    Added a class Solution with method longestCommonPrefix to find the longest common prefix among strings.

--- l14.py
+++ l14.py
@@ -15,3 +15,20 @@ class Solution(object):
         return ret
                     
 #This is a very slow implementation due to sorting done for each item, but I will put a code without sorting in the bottom -->
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        s_strs = sorted(strs, key = len)
+        ret = s_strs[0]
+        for i in range(len(s_strs)):
+            if i+1<len(s_strs):
+                temp = ""
+                for k in range(len(ret)):
+                    if ret[k] == s_strs[i+1][k]:
+                        temp+= s_strs[i+1][k]
+                    else: 
+                        break 
+                if len(temp)<len(ret):
+                    ret = temp
+        return ret
+# 2% faster
